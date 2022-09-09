2c54668004a96c9008149b0bda121445a946ed68
Cinthia Tengan <cinthia.tengan@ab-inbev.com>
2022-09-09 11:25:08 -0300

    first pull

---
+++ CI/scripts/build_and_push.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+cd etl/
+
+# Push to AWS ECR
+aws ecr get-login-password --region sa-east-1 | docker login --username AWS --password-stdin 944471524566.dkr.ecr.sa-east-1.amazonaws.com
+docker build -t igti-ney-prod-extract-rais .
+docker tag igti-ney-prod-extract-rais:latest 944471524566.dkr.ecr.sa-east-1.amazonaws.com/igti-ney-prod-extract-rais:latest
+docker push 944471524566.dkr.ecr.sa-east-1.amazonaws.com/igti-ney-prod-extract-rais:latest
---
+++ infrastructure/aws/variables.tf
@@ -0,0 +1,48 @@
+variable "region_id" {
+  default = "us-east-2"
+}
+
+variable "prefix" {
+  default = "cin-rais"
+}
+
+variable "account" {
+  default = 944471524566
+}
+
+# Prefix configuration and project common tags
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
+  common_tags = {
+    Project      = "RAIS"
+    ManagedBy    = "Terraform"
+    Department   = "XPed",
+    Provider     = "A3DATA",
+    Owner        = "Data Engineering"
+    BusinessUnit = "Data"
+    Billing      = "Infrastructure"
+    Environment  = terraform.workspace
+    UserEmail    = "neylson.crepalde@a3data.com.br"
+  }
+}
+
+variable "bucket_names" {
+  description = "Create S3 buckets with these names"
+  type        = list(string)
+  default = [
+    "landing-zone",
+    "processing-zone",
+    "delivery-zone"
+  ]
+}
+
+variable "database_names" {
+  description = "Create databases with these names"
+  type        = list(string)
+  default = [
+    #landing-zone
+    "dl_landing_zone",
+    "dl_processing_zone",
+    "dl_delivery_zone"
+  ]
+}
